148ea7cd3222dcf9d55f77b03503329408763bce
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2023-04-27 01:56:45 -0300

    feat: implement query client provider and app navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from "react";
+import { StyleSheet } from "react-native";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { CartProvider } from "./src/context/CartContext";
+import ProductsList from "./src/screens/ProductList/ProductList";
+import ProductDetails from "./src/screens/ProductDetails/ProductDetails";
+import Cart from "./src/screens/Cart/Cart";
+import CartIcon from "./src/components/CartIcon/CartIcon";
+
+type RootStackParamList = {
+  Products: {};
+  ProductDetails: { productId: number };
+  Cart: {};
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <CartProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Products"
+              component={ProductsList}
+              options={({ navigation }) => ({
+                title: "Products",
+                headerTitleStyle: styles.headerTitle,
+                headerRight: () => <CartIcon navigation={navigation} />,
+              })}
+            />
+            <Stack.Screen
+              name="ProductDetails"
+              component={ProductDetails}
+              options={({ navigation }) => ({
+                title: "Product details",
+                headerTitleStyle: styles.headerTitle,
+                headerRight: () => <CartIcon navigation={navigation} />,
+              })}
+            />
+            <Stack.Screen
+              name="Cart"
+              component={Cart}
+              options={({ navigation }) => ({
+                title: "My cart",
+                headerTitleStyle: styles.headerTitle,
+                headerRight: () => <CartIcon navigation={navigation} />,
+              })}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </CartProvider>
+    </QueryClientProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  headerTitle: {
+    fontSize: 20,
   },
 });
