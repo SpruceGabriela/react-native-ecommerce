d45c87b1e120e911906320c1d025b3e984591b96
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2023-04-27 22:04:36 -0300

    feat: add new UI redesign style to cart icon

--- src/components/CartIcon/CartIcon.style.ts
+++ src/components/CartIcon/CartIcon.style.ts
@@ -2,16 +2,27 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    marginHorizontal: 8,
-    backgroundColor: "orange",
-    height: 32,
-    padding: 12,
-    borderRadius: 32 / 2,
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  counter: {
+    minWidth: 20,
+    backgroundColor: "#bf4345",
+    padding: 4,
+    borderRadius: 10,
+    display: "flex",
     alignItems: "center",
     justifyContent: "center",
+    position: "absolute",
+    right: -10,
+    top: -5,
+    zIndex: 9,
   },
   text: {
     color: "white",
     fontWeight: "bold",
+    fontSize: 10,
   },
 });
